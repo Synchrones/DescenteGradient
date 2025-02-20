d0d30e3221352b485ba6d4946f856da8cd40f4c8
Salsabanana <Axelchanoine@gmail.com>
2025-02-20 18:21:50 +0100

    descente x manim

---
+++ manim/IllustrationDescente.py
@@ -0,0 +1,58 @@
+import random
+
+from manim import *
+from pyglet.window import *
+
+depart = []
+derives = [lambda x: float((np.cos(x) - x * np.sin(x))/ x**2)]
+nouvelles_coords = []
+point = None
+axes = None
+
+def fnc(x):
+    return float(np.sin(x) / x)
+
+class DescenteGradient(Scene):
+
+    def construct(self):
+        global depart
+        global derives
+        global nouvelles_coords
+        global point
+        global axes
+        axes = Axes(x_range=(-10, 10), y_range=(-1, 1))
+        labels = axes.get_axis_labels(x_label="x", y_label="y")
+        courbe = axes.plot(fnc, color=YELLOW)
+        depart = [random.randint(-5, 5)]
+        nouvelles_coords = depart[:]
+        point = Dot(point=axes.coords_to_point(depart[0], fnc(depart[0])))
+        self.play(Create(axes, run_time=3), Create(labels), Create(courbe, run_time=3))
+        self.add(point)
+
+        self.interactive_embed()
+
+
+
+    def on_key_press(self, symbol, modifiers):
+        global depart
+        global derives
+        global nouvelles_coords
+        global point
+        global axes
+        if symbol == key.SPACE:
+            termine = True
+            print(depart)
+            pentes = [derives[i](depart[i]) for i in range(len(derives))]
+            for var in range(len(pentes)):
+                nouvelles_coords[var] = depart[var] - pentes[var] * 0.4
+                print(nouvelles_coords, depart, pentes)
+                courbe_partielle = axes.plot(fnc, x_range=[min(depart[0], nouvelles_coords[0]),
+                                                           max(depart[0], nouvelles_coords[0]),
+                                                           abs((depart[0] + nouvelles_coords[0])) / 10])
+                if nouvelles_coords[0] - depart[0] > 0:
+                    self.play(MoveAlongPath(point, courbe_partielle), run_time=1, rate_func=smooth)
+                else:
+                    self.play(MoveAlongPath(point, courbe_partielle.reverse_points()), run_time=0.5, rate_func=smooth)
+                depart = nouvelles_coords[:]
+                if abs(pentes[var]) > 0.1:
+                    termine = False
