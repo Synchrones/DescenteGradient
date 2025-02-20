ed8b855be0d9f834c189daace7eb12383697dc3a
Salsabanana <Axelchanoine@gmail.com>
2025-02-20 12:56:23 +0100

    bug fix

--- algos/DescenteClassique.py
+++ algos/DescenteClassique.py
@@ -8,14 +8,18 @@ def descente_gradient(fnc, derives, depart, epsilon, pas):
     IN : fnc (function) - la fonction étudiée
     IN : derives (list(function)) - les dérivées selon chaque variable de la fonction
     IN : depart (list(float)) - le point de départ de la descente
-    IN : epsilon (float) - la valeur à partir de laquelle on arrête la descente (on atteint pas forcément le 0)
-    IN : pas (float) - la vitesse de convergence (ne doit pas etre trop gros ou trop petit)
+    IN : epsilon (float) - la valeur de la dérivée à partir de laquelle on arrête la descente (on n'atteint pas forcément le 0)
+    IN : pas (float) - la vitesse de convergence (ne doit pas être trop gros ou trop petit)
     """
     iterations = 0
-    while abs(fnc(*depart)) > epsilon:
+    terminer = False
+    while not terminer:
+        terminer = True
         pentes = [derives[i](depart[i]) for i in range(len(derives))]
         for var in range(len(pentes)):
             depart[var] -= pentes[var] * pas
+            if abs(pentes[var]) > epsilon:
+                terminer = False
         iterations += 1
         print(depart)
     return depart, iterations
@@ -25,4 +29,4 @@ print(descente_gradient(lambda x, y: x**2 + y**2,
                         [lambda x: 2*x, lambda y: 2*y],
                         [random.randint(-100, 100), random.randint(-100, 100)],
                         0.1,
-                        0.1))
+                        0.4))
